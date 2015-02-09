2c4fcc71c09930065c2db53624fe307fa42e80c1
KingdomHearts <michael_bossink@hotmail.com>
2015-02-09 12:47:33 +0100

    Commit voor Prototype and LevelController

---
+++ 0.0.5/sample_project/LevelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class LevelController : GameObject
+    {
+        public LevelController()
+        {
+
+        }
+
+        public void LevelState(/*enum of andere mogelijk om level te bepalen*/)
+        {
+        }
+
+        private void LevelReader(/*enum of andere mogelijk om level te bepalen*/)
+        {
+            //Vanuit LevelState het bestand meesturen dat hierin wordt opgehaald.
+        }
+
+        private void LevelBuilder(/* array van XML */)
+        {
+            //De array wordt hierin uitgelezen en gebouwd. Vanuit deze wordt de bijpassende CS aangeroepen waar de rest van de events wordt uitgevoerd.
+        }
+    }
+}
